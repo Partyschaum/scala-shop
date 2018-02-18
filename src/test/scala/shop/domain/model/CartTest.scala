8295fc891eb79c03e5a25b16b0c5b36159edf3a4
Hauke Stange <partyschaum@posteo.de>
2018-02-18 16:27:38 +0100

    Uses fixture in CartTest

--- src/test/scala/shop/domain/model/CartTest.scala
+++ src/test/scala/shop/domain/model/CartTest.scala
@@ -6,17 +6,21 @@ import org.scalatest.FunSpec
 
 class CartTest extends FunSpec {
 
+  def fixture = new {
+    val aCart = new Cart(new UserId(UUID.randomUUID()))
+  }
+
   describe("Cart") {
     describe("when initially created") {
       it("contains no items") {
-        val cart = new Cart(new UserId(UUID.randomUUID()))
+        val cart = fixture.aCart
         assert(cart.items().isEmpty)
       }
     }
 
     describe("after adding two items") {
       it("contains two items") {
-        val cart = new Cart(new UserId(UUID.randomUUID()))
+        val cart = fixture.aCart
         cart.addItem(new Item("Chocolate", 1.99))
         cart.addItem(new Item("Peanuts", 2.49))
         assert(cart.items().lengthCompare(2) == 0)
